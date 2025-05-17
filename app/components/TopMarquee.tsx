c6e5abf71a975966704c06e4adfc7b9456ccf11b
Shivansh Chopra <gauravshivansh7@gmail.com>
2025-05-17 14:16:34 +0530

    adding top marquee

---
+++ app/components/TopMarquee.tsx
@@ -0,0 +1,14 @@
+const TopMarquee = () => {
+  return (
+    <div className="w-full overflow-hidden bg-black text-white text-sm font-semibold marquee-wrapper">
+      <i className="marquee-inner">
+        &nbsp;&nbsp;ASIA’S #1 HYPE AND LUXURY APP&nbsp;&nbsp;&nbsp;&nbsp;SHARK SALE&nbsp;&nbsp;&nbsp;&nbsp;BEST PRICES EVER ON THE ENTIRE COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;
+        ASIA’S #1 HYPE AND LUXURY APP&nbsp;&nbsp;&nbsp;&nbsp;SHARK SALE&nbsp;&nbsp;&nbsp;&nbsp;BEST PRICES EVER ON THE ENTIRE COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;ASIA’S #1 HYPE AND LUXURY APP&nbsp;&nbsp;&nbsp;&nbsp;SHARK SALE&nbsp;&nbsp;&nbsp;&nbsp;BEST PRICES EVER ON THE ENTIRE COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;
+        ASIA’S #1 HYPE AND LUXURY APP&nbsp;&nbsp;&nbsp;&nbsp;SHARK SALE&nbsp;&nbsp;&nbsp;&nbsp;BEST PRICES EVER ON THE ENTIRE COLLECTION&nbsp;&nbsp;&nbsp;&nbsp;
+      </i>
+    </div>
+  );
+};
+
+export default TopMarquee;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import TopMarquee from './components/TopMarquee'
 
 export const metadata: Metadata = {
   title: 'Culture Circle',
@@ -16,7 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        
         <Navbar />
+        <TopMarquee />
         <main>{children}</main>
          <Footer />
       </body>
